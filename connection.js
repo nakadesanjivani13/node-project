5f81db8da56e7352d7565ff4c8009745734e56ab
Sanjivani Nakade <nakadesanjivani13@gmail.com>
2026-01-09 10:22:40 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ var util = require("util");
 
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "ultra_project",
+    host: "bpxx3bi5ggviysexvwtb-mysql.services.clever-cloud.com",
+    user: "ukfnikjgc4vnbpkp",
+    password: "A0ZVT5r5fSYQT16uhqVX",
+    database: "bpxx3bi5ggviysexvwtb",
 })
 const exe = util.promisify(conn.query).bind(conn);
 
